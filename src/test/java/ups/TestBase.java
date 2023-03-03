d64248ca99ecf8fab4a7f62226ed67df094f3dd2
Olgaidukova <olya.gaidukova@gmail.com>
2023-03-03 14:11:16 +0300

    add config

--- src/test/java/ups/TestBase.java
+++ src/test/java/ups/TestBase.java
@@ -1,13 +1,11 @@
 package ups;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.UpsPage;
 
 
@@ -16,16 +14,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://www.ups.com";
-        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
-        Configuration.browser = System.getProperty("browser","chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion","100.0");
-        Configuration.remote = System.getProperty("remote","https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+        WebDriverProvider provider = new WebDriverProvider();
     }
     @BeforeEach
     void addListner() {
